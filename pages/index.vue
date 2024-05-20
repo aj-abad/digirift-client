e65aece0a2f931dc661f0e2926b3e9814c66a2c4
aj-enk <136690481+aj-enk@users.noreply.github.com>
2024-05-20 17:03:49 +0800

    Add login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,55 @@
 <template>
+  <div
+    class="d-flex align-center justify-center bg-blue-grey-lighten-5 h-100 w-100 overflow-hidden">
+    <VSheet class="pa-4 bg-white rounded-lg border" id="login-sheet">
+      <h1 class="mb-4">Login to continue</h1>
+      <VAlert v-if="errorMessage" type="error" class="mb-4" variant="tonal" border="start">
+        {{ errorMessage }}
+      </VAlert>
+      <form @submit.prevent="() => login({ email, password })" class="d-flex gap-2 flex-column">
+        <VTextField variant="underlined" v-model="email" type="email" label="Email" outlined />
+        <VTextField
+          variant="underlined"
+          v-model="password"
+          label="Password"
+          outlined
+          type="password" />
+        <VBtn
+          :aria-busy="loading"
+          aria-label="Login"
+          :disabled="loading || !email || !password"
+          type="submit"
+          variant="flat"
+          color="teal-darken-4">
+          <span v-if="!loading">Login</span>
+          <span v-else>
+            <VProgressCircular indeterminate color="white" size="24" />
+          </span>
+        </VBtn>
+      </form>
+    </VSheet>
+  </div>
 </template>
 
 <script setup>
+import { VAlert } from "vuetify/components";
+
+const email = ref("");
+const password = ref("");
+
+const query = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(input: { email: $email, password: $password }) {
+      token
+    }
+  }
+`;
+const { mutate: login, error, loading } = useMutation(query);
+const errorMessage = computed(() => error.value?.graphQLErrors?.map((e) => e.message).join(", "));
 </script>
+
+<style lang="scss">
+#login-sheet {
+  width: 32rem;
+}
+</style>
